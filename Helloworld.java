edfa4547dc527a5981519bfeed4a9cc7ff061d0d
unknown <anantkreshnav@gmail.com>
2018-09-10 21:51:13 +0530

    Modified the print statement

--- Helloworld.java
+++ Helloworld.java
@@ -2,7 +2,7 @@ public class Helloworld {
 
 public static void main(String[] args) {
 
-System.out.println("Hello Vidyalakshmi Anantkreshna ...");
+System.out.println("Hello Vidyalakshmi Anantkreshna V ...");
 
 }
 
